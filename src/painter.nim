cc066de2173c71f302eb2dc192166127c6669fdd
swag31415 <kidswagatam18@gmail.com>
2020-07-08 16:33:48 -0400

    Added a method of getting the dominant pixels in a kernel

--- src/painter.nim
+++ src/painter.nim
@@ -22,3 +22,12 @@ proc diff(c1, c2: Pixel): int =
   (c1.color.g.int - c2.color.g.int)^2 +
   (c1.color.b.int - c2.color.b.int)^2 +
   (c1.color.a.int - c2.color.a.int)^2
+
+proc get_dom_pixs(k: Kernel): seq[Pixel] =
+  var s = (@k).sortedByIt(it.diff(k[0])) # Sort all of them by their dist to a random node (the pode)
+  if s[0].diff(s[^1]) == 0: return s
+
+  result.add(s.pop()) # Get the node furthest from the pode (the antipode)
+  while s[^1].diff(result[0]) < s[^1].diff(s[0]): # if a node is closer to the antipode than the pode
+    result.add(s.pop()) # Add it to the result
+  if s.len > (k.len div 2): return s # if we have a majority in `s` return `s`
