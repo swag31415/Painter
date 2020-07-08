2edd9400958ddfd023474f82cd41036e5a9ae5c9
swag31415 <kidswagatam18@gmail.com>
2020-07-08 16:29:18 -0400

    Added way of comparing two pixels (not named cmp intentionally)

--- src/painter.nim
+++ src/painter.nim
@@ -2,6 +2,8 @@
 
 import flippy, chroma
 
+import sequtils, sugar, algorithm, math
+
 type
   Pixel = tuple[x, y: int; color: ColorRGBA]
   Kernel = array[3*3, Pixel]
@@ -14,3 +16,9 @@ iterator kernels(img: Image): Kernel =
         for j in -1..1:
           k[3*(i+1) + (j+1)] = (x+i, y+j, img.getRgba(x+i, y+j))
       yield k
+
+proc diff(c1, c2: Pixel): int =
+  (c1.color.r.int - c2.color.r.int)^2 +
+  (c1.color.g.int - c2.color.g.int)^2 +
+  (c1.color.b.int - c2.color.b.int)^2 +
+  (c1.color.a.int - c2.color.a.int)^2
